2498bfdc161f3bbb271f69a9168a477142caf20d
AK-CHMC <111881019+AK-CHMC@users.noreply.github.com>
2023-03-14 19:51:58 -0400

    Update gpu_job_sample.bat

--- gpu_job_sample.bat
+++ gpu_job_sample.bat
@@ -7,11 +7,13 @@
 #BSUB -J nextmod
 #BSUB -e /users/kin8hb/%J.err
 #BSUB -o /users/kin8hb/%J.err
+
 module load anaconda3
 module load cuda/10.1
 module load cuda/10.2
 module load cuda/11.2
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-10.0/lib64/
+
 source activate tensorflow-2
+
 jupyter nbconvert --to python "/users/kin8hb/Spring_2023/Programs/multifunction_ensemble/train_next_ensemble_model.ipynb"
 python "/users/kin8hb/Spring_2023/Programs/multifunction_ensemble/train_next_ensemble_model.py"
